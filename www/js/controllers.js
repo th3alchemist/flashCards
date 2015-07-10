495d611ea152c97b46fba56c2c3ff2fefd946c2d
th3alchemist <ayo.j.arum@gmail.com>
2015-07-10 15:57:48 -0400

    modified reviewctrl to read from json file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('ReviewCtrl', function($scope) {})
-
+.controller('ReviewCtrl', function($scope, $http) {
+	$http.get("res/greetings.json")
+		.success(function(data){
+			$scope.cardList = data;
+		});
+})
 .controller('QuizCtrl', function($scope) {})
 
 .controller('StudyCtrl', function($scope) {});
