dd83006f78cd66a1a1e2837f5b8b6198ff423086
th3alchemist <ayo.j.arum@gmail.com>
2015-07-10 16:52:26 -0400

    added logic to drop down menus; menus are p[opulated from json keys

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,18 @@
 angular.module('starter.controllers', [])
 
 .controller('ReviewCtrl', function($scope, $http) {
-	$http.get("res/daysOfTheWeek.json")
+	$http.get("res/greetings.json")
 		.success(function(data){
 			$scope.cardList = data;
+			var optStr = ""
+			var newOpt = Object.keys(data[0]); //retrieve the first card as a reference
+
+			for (var i = 0; i < newOpt.length; i++) {
+			    optStr = optStr + "<option value='" + newOpt[i] + "'>" + newOpt[i] + "</option>\n";
+			}
+
+			document.getElementById('cardFrontSelection').innerHTML = optStr;
+			document.getElementById('cardBackSelection').innerHTML = optStr;
 		});
 })
 .controller('QuizCtrl', function($scope) {})
