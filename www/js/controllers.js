be38fa75cc75ec5731f3eff40e564cf48f97a37a
th3alchemist <ayo.j.arum@gmail.com>
2015-07-13 15:11:13 -0400

    added logic to forward and back arrows on review page.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,24 @@
 angular.module('starter.controllers', [])
 
 .controller('ReviewCtrl', function($scope, $http, $stateParams) {
-	$http.get("res/greetings.json")
+	$http.get("res/daysOfTheWeek.json")
 		.success(function(data){
-
-			$scope.index = $stateParams.cardId;
-			$scope.cardList = data;
+			var frontEle = document.getElementById('cardFrontSelection');
+			var backEle = document.getElementById('cardBackSelection');
 			var optStr = ""
+
 			updateOptionMenu(data[0]);
-		});
+
+			$scope.cardList = data;
+			$scope.index = $stateParams.cardId;
+			$scope.nextCard = (parseInt($stateParams.cardId)+1) % data.length;
+			$scope.prevCard = (parseInt($stateParams.cardId)-1) % data.length;
+			if ($scope.prevCard <0){
+				$scope.prevCard = data.length-1;
+			}
+			$scope.front = frontEle.options[frontEle.selectedIndex].text;
+			$scope.back = backEle.options[backEle.selectedIndex].text;
+		});	
 })
 .controller('QuizCtrl', function($scope) {})
 
